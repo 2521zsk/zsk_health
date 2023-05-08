b82d8cae4c3301ade11ee18d7adc067c7440494a
2521zsk <2521827891@qq.com>
2023-05-08 17:08:50 +0800

    2023-05-08：为定时清理七牛云多余图片提供条件 使用Redis存储，SETMEAL_PIC_RESOURCES和SETMEAL_PIC_DB_RESOURCES分别存储上传的图片以及确切添加到数据库的图片，之后定时清理两个集合的差值

--- health_parent/health_backen/src/main/java/com/zsk/controller/SetmealController.java
+++ health_parent/health_backen/src/main/java/com/zsk/controller/SetmealController.java
@@ -2,15 +2,18 @@ package com.zsk.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zsk.constant.MessageConstant;
+import com.zsk.constant.RedisConstant;
 import com.zsk.entity.Result;
 import com.zsk.pojo.Setmeal;
 import com.zsk.service.SetmealService;
 import com.zsk.utils.QiNiuUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import redis.clients.jedis.JedisPool;
 
 import java.util.UUID;
 
@@ -20,6 +23,10 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/setmeal")
 public class SetmealController {
+    //使用JedisPool操作Redis服务
+    @Autowired
+    private JedisPool jedisPool;
+
     @Reference
     private SetmealService setmealService;
 
@@ -38,6 +45,8 @@ public class SetmealController {
             //图片上传成功
             Result result = new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS);
             result.setData(fileName);
+            //将上传图片名称存入Redis，基于Redis的Set集合存储
+            jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_RESOURCES, fileName);
             return result;
         } catch (Exception e) {
             e.printStackTrace();
--- health_parent/health_service_provider/src/main/java/com/zsk/service/impl/SetmealServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/zsk/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,13 @@
 package com.zsk.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.zsk.constant.RedisConstant;
 import com.zsk.dao.SetmealDao;
 import com.zsk.pojo.Setmeal;
 import com.zsk.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.JedisPool;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +15,8 @@ import java.util.Map;
 @Service(interfaceClass = SetmealService.class)
 @Transactional
 public class SetmealServiceImpl implements SetmealService {
+    @Autowired
+    private JedisPool jedisPool;
     @Autowired
     private SetmealDao setmealDao;
 
@@ -30,5 +34,7 @@ public class SetmealServiceImpl implements SetmealService {
                 setmealDao.setSetmealAndCheckgroup(map);
             }
         }
+        //将图片名称保存到Redis
+        jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_DB_RESOURCES, setmeal.getImg());
     }
 }
